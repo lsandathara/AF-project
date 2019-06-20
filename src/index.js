cefe4dec77de65d7dbd6fb3fbf048f0151d48d89
lsandathara <dmlakchani@gmail.com>
2019-06-20 18:51:03 +0530

    student function - CRUD

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//import myCourse from '../src/component/Student/myCourses';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
